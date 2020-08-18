3505e8160ff1565673606d68066a98814e39942a
kunalkr789 <55917079+kunalkr789@users.noreply.github.com>
2020-08-18 20:13:49 +0530

    Updates

---
+++ precache-manifest.7172f1ee6af7f403f9aa7368776282a9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d912d446f6eb4afd5ee5139fd1267f1b",
+    "url": "/qutiq-assignment/index.html"
+  },
+  {
+    "revision": "93cf53e4e833565a2601",
+    "url": "/qutiq-assignment/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "57fadaee77d916721046",
+    "url": "/qutiq-assignment/static/css/main.df57fccd.chunk.css"
+  },
+  {
+    "revision": "93cf53e4e833565a2601",
+    "url": "/qutiq-assignment/static/js/2.0e978a22.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/qutiq-assignment/static/js/2.0e978a22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57fadaee77d916721046",
+    "url": "/qutiq-assignment/static/js/main.55ab7df1.chunk.js"
+  },
+  {
+    "revision": "525ee68db20918ae769e",
+    "url": "/qutiq-assignment/static/js/runtime-main.a99ba4d3.js"
+  }
+]);
